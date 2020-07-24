4edee05aa67fe6d647b2ab137635e35b41aadfb7
johanuribe03 <johan.uribe@gmail.com>
2020-07-24 11:40:40 +0000

    card update

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,23 +3,25 @@ import PropTypes from "prop-types";
 
 //create your first component
 export function Card(props) {
-    const {title, body, buttonText, url, image} = props;
+	const { title, body, buttonText, url, image } = props;
 	return (
-        <div className="card">
-            <img className="card-img-top" src={image} alt="Card image cap" />
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{body}</p>
-                <a href={url} className="btn btn-primary">{buttonText}</a>
-            </div>
-            </div>
+		<div className="card">
+			<img className="card-img-top" src={image} alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">{title}</h5>
+				<p className="card-text">{body}</p>
+				<a href={url} className="btn btn-primary">
+					{buttonText}
+				</a>
+			</div>
+		</div>
 	);
 }
 
 Card.propTypes = {
-    tittle: PropTypes.string,
-    body: PropTypes.string,
-    buttonText: PropTypes.string,
-    url: PropTypes.string,
-    image: PropTypes.string
-}
+	title: PropTypes.string,
+	body: PropTypes.string,
+	buttonText: PropTypes.string,
+	url: PropTypes.string,
+	image: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,67 +1,72 @@
-import Navbar from "./Navbar.js";
-import Jumbotron from "./jumbotron.js";
-import Card from "./card.js";
-import Footer from "./footer.js";
+import React from "react";
+// import { Navbar } from "./navbar.js";
+// import { Jumbo } from "./jumbo.js";
+import { Card } from "./card.js";
+// import { Footer } from "./footer.js";
 
+export default function Home() {
+	let cards = [
+		{
+			image: "https://via.placeholder.com/300",
+			title: "My Card",
+			body:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ",
+			buttonText: "View",
+			url: "#"
+		},
 
+		{
+			image: "https://via.placeholder.com/300",
+			title: "My Card",
+			body:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ",
+			buttonText: "View",
+			url: "#"
+		},
+		{
+			image: "https://via.placeholder.com/300",
+			title: "My Card",
+			body:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ",
+			buttonText: "View",
+			url: "#"
+		},
+		{
+			image: "https://via.placeholder.com/300",
+			title: "My Card",
+			body:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ",
+			buttonText: "View",
+			url: "#"
+		},
+		{
+			image: "https://via.placeholder.com/300",
+			title: "My Card",
+			body:
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ",
+			buttonText: "View",
+			url: "#"
+		}
+	];
 
-export default function Home () {
-
-
-    let cards = [
-
-{   
-    image: "https://via.placeholder.com/300", 
-    title: "My Card", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ", 
-    buttonText: "View", 
-    url: "#" 
+	return (
+		<div className="container-fluid">
+			{/* <Navbar /> */}
+			{/* <Jumbo /> */}
+			<div className="row">
+				{cards.map((card, index) => (
+					<div className="col-3" key={index}>
+						<Card
+							tittle={card.title}
+							body={card.body}
+							buttonText={card.buttonText}
+							url={card.url}
+							image={card.image}
+						/>
+					</div>
+				))}
+			</div>
+			{/* <Footer /> */}
+		</div>
+	);
 }
-
-{   
-    image: "https://via.placeholder.com/300", 
-    title: "My Card", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ", 
-    buttonText: "View", 
-    url: "#" 
-}
-{   
-    image: "https://via.placeholder.com/300", 
-    title: "My Card", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ", 
-    buttonText: "View", 
-    url: "#" 
-}
-{   
-    image: "https://via.placeholder.com/300", 
-    title: "My Card", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ", 
-    buttonText: "View", 
-    url: "#" 
-}
-{   
-    image: "https://via.placeholder.com/300", 
-    title: "My Card", 
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rutrum in metus quis pellentesque. ", 
-    buttonText: "View", 
-    url: "#" 
-}
-
-
-
-    return (
-        <div className="container-fluid">
-        <Navbar />
-        <Jumbotron />
-        <div className="row">
-        {   card.map((card,index)) => 
-            <div className="col-3" key={index}>
-                <Card tittle={card.title} body={card.body} buttonText={card.buttonText} url={card.url}, image={card.image}
-}
-            </div>
-
-        }
-        </div>
-
-        </div>
-    )
