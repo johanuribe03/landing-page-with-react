d667804a15de5ace662fa97178bd8a600fe24166
johanuribe03 <johan.uribe@gmail.com>
2020-07-24 11:43:18 +0000

    navbar1

---
+++ src/js/component/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+//create your first component
+export function Navbar() {
+	// const { title, body, buttonText, url, image } = props;
+	return (
+		
+	);
+}
+
+// Navbar.propTypes = {
+// 	title: PropTypes.string,
+// 	body: PropTypes.string,
+// 	buttonText: PropTypes.string,
+// 	url: PropTypes.string,
+// 	image: PropTypes.string
+// };
